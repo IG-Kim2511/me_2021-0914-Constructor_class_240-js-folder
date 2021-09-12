5c0785cb68ec4b0198f953f8f5a82c96575214d3
iikim <iikim2511@gmail.com>
2021-09-12 13:02:37 -0400

    Constructor + for Loop

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 
-function Character(params) {
+const stageElem = document.querySelector(".stage");
+
+// let num;
+
+// 🍀Constructor
+
+/*
+🍄hard coding
+
+function Character(a_num) {
+    
     this.mainElem = document.createElement("div");
     this.mainElem.classList.add('character');
     this.mainElem.innerHTML =`
-    
+        <img src="./img/poke(${a_num}).png" alt="">
     `;
+    stageElem.appendChild(this.mainElem);
+}
+new Character(1);     */
+
+
+//🍄soft coding + for loop
+function Character(a_num) {
+    
+    this.mainElem = document.createElement("div");
+    this.mainElem.classList.add('character');
+    this.mainElem.innerHTML =`
+        <img src="./img/poke(${a_num}).png" alt="">
+    `;
+    stageElem.appendChild(this.mainElem);
 }
 
-
-new Character();
+for (let i = 1; i < 6; i++) {
+    let num = i;
+    new Character(num);    
+}
