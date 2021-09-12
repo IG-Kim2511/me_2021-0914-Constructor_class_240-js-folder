49a0807b4b30b5dfc51f22a01ef9108495927ba0
iikim <iikim2511@gmail.com>
2021-09-12 12:13:06 -0400

    (wip)

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function Character(params) {
+    this.mainElem = document.createElement("div");
+    this.mainElem.classList.add('character');
+    this.mainElem.innerHTML =`
+    
+    `;
+}
+
+
+new Character();
